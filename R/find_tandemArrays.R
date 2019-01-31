bf13c3d9b88721f8443e44e644dc60ada95a2bcb
LovellHAGSC <jlovell@hudsonalpha.org>
2019-01-30 19:52:09 -0700

    v0.3 updates

--- R/find_tandemArrays.R
+++ R/find_tandemArrays.R
@@ -30,26 +30,20 @@
 #' @import data.table
 #' @importFrom compiler cmpfun
 #' @export
-find_pairwiseTandemArrays <- function(gff,
+find_collinearArrays <- function(gff,
                                       blast,
                                       mcscan.dir,
-                                      orthogroup.dt,
                                       rerank = T,
                                       DCTA.path = "/Users/jlovell/Documents/comparative_genomics/programs/MCScanX/downstream_analyses/detect_collinear_tandem_arrays",
                                       verbose = T,
-                                      m.param = 50,
-                                      w.param = 10,
-                                      s.param = 10,
-                                      b.param = 0,
-                                      k.param = 10,
-                                      e.param = .1,
+                                      MCScanX.param = "-m 50 -w 10 -s 10 -b 0 -k",
                                       ...){
   #######################################################
   #######################################################
-  prep_gff4mcscan <- function(gff, genomeIDs){
+  prep_gff4mcscan <- function(gff,
+                              genomeIDs){
     gff <- gff[gff$genome %in% genomeIDs,]
-    gff[,chr.num := frank(chr,
-                          ties.method = "dense"),
+    gff[,chr.num := frank(chr, ties.method = "dense"),
             by = genome]
     gff$genome <- factor(gff$genome,
                          levels = genomeIDs)
@@ -59,36 +53,40 @@ find_pairwiseTandemArrays <- function(gff,
                          ties.method = "dense")
     gff[,genome.num := frank(genome,
                              ties.method = "dense")]
-    lets = paste0(letters,letters)[1:length(unique(gff$genome))]
-    gff$genome.abbrev = paste0(lets[gff$genome.num],gff$chr.num)
-    gff = gff[,c("genome.abbrev","id","start","end")]
+    lets <- paste0(letters,letters)[1:length(unique(gff$genome))]
+    gff$genome.abbrev <- paste0(lets[gff$genome.num], gff$chr.num)
+    gff <- gff[,c("genome.abbrev", "id", "start", "end")]
     return(gff)
   }
   #######################################################
   #######################################################
-  prep_blast4mcscan <- function(blast, genomeIDs){
+  prep_blast4mcscan <- function(blast,
+                                genomeIDs){
     blast <- blast[with(blast,
                         genome2 %in% genomeIDs &
-                          genome1 %in% genomeIDs),]
-    blast <- blast[,c("id1","id2","perc.iden","align.length",
-                            "n.mismatch","n.gapOpen", "q.start", "q.end",
-                            "s.start", "s.end","eval","score")]
+                          genome1 %in% genomeIDs), ]
+    blast <- blast[,c("id1", "id2",
+                      "perc.iden", "align.length",
+                      "n.mismatch", "n.gapOpen",
+                      "q.start", "q.end",
+                      "s.start", "s.end",
+                      "eval", "score")]
     return(blast)
   }
   #######################################################
   #######################################################
   propagate_connectedDCTAs <- function(dcta.dt){
-    aids2check = unique(dcta.dt$array.id[duplicated(dcta.dt$og) |
+    aids2check <- unique(dcta.dt$array.id[duplicated(dcta.dt$og) |
                                            duplicated(dcta.dt$og, fromLast = T)])
-    while(length(aids2check)>0){
-      x = aids2check[1]
-      tid = dcta.dt$array.id[dcta.dt$id %in% dcta.dt$id[dcta.dt$array.id %in% x]]
-      tod = vector()
-      while(length(tod)<length(tid)){
-        tod = tid
-        tid = dcta.dt$array.id[dcta.dt$id %in% dcta.dt$id[dcta.dt$array.id %in% tid]]
+    while (length(aids2check) > 0) {
+      x <- aids2check[1]
+      tid <- dcta.dt$array.id[dcta.dt$id %in% dcta.dt$id[dcta.dt$array.id %in% x]]
+      tod <- vector()
+      while (length(tod) < length(tid)) {
+        tod <- tid
+        tid <- dcta.dt$array.id[dcta.dt$id %in% dcta.dt$id[dcta.dt$array.id %in% tid]]
       }
-      dcta.dt$array.id[dcta.dt$array.id %in% unique(tid)]<-tid[1]
+      dcta.dt$array.id[dcta.dt$array.id %in% unique(tid)] <- tid[1]
       aids2check <- aids2check[!aids2check %in% tid]
     }
     dcta.dt <- dcta.dt[!duplicated(dcta.dt),]
@@ -97,20 +95,20 @@ find_pairwiseTandemArrays <- function(gff,
   #######################################################
   #######################################################
   propagate_connectedOGs <- function(dcta.dt){
-    ud = dcta.dt[!duplicated(dcta.dt[,c("og","array.id")]),]
-    dup = duplicated(ud$og)
+    ud <- dcta.dt[!duplicated(dcta.dt[,c("og","array.id")]),]
+    dup <- duplicated(ud$og)
 
     aids2check <- ud$array.id[which(dup)]
 
-    while(length(aids2check)>0){
+    while (length(aids2check) > 0) {
       x = aids2check[1]
       tid = dcta.dt$array.id[dcta.dt$og %in% dcta.dt$og[dcta.dt$array.id %in% x]]
       tod = vector()
-      while(length(tod)<length(tid)){
+      while (length(tod) < length(tid)) {
         tod = tid
         tid = dcta.dt$array.id[dcta.dt$og %in% dcta.dt$og[dcta.dt$array.id %in% tid]]
       }
-      dcta.dt$array.id[dcta.dt$array.id %in% unique(tid)]<-tid[1]
+      dcta.dt$array.id[dcta.dt$array.id %in% unique(tid)] <- tid[1]
       aids2check <- aids2check[!aids2check %in% tid]
     }
     dcta.dt <- dcta.dt[!duplicated(dcta.dt),]
@@ -118,23 +116,28 @@ find_pairwiseTandemArrays <- function(gff,
   }
   #######################################################
   #######################################################
-  load_DCTAs <- function(dtca.output, orthogroup.dt){
-    raw = fread(dtca.output, header = T)
-    r2 = apply(raw, 1, function(x)
+  load_DCTAs <- function(dtca.output,
+                         map){
+    og.dt <- with(map,
+                 data.table(id = c(id1, id2),
+                            og = c(og1, og2)))
+    og.dt <- og.dt[!duplicated(og.dt),]
+    raw <- fread(dtca.output, header = T)
+    r2 <- apply(raw, 1, function(x)
       strsplit(
         paste(x[1], x[3],
               x[2], x[4],
-              sep = ","),
-        ",")[[1]])
-    for(i in 1:length(r2))
-      r2[[i]]<-data.table(id = r2[[i]],
+              sep = ","), ",")[[1]])
+
+    for (i in 1:length(r2))
+      r2[[i]] <- data.table(id = r2[[i]],
                           array.id = i)
-    r2 = rbindlist(r2)
+    r2 <- rbindlist(r2)
     r2 <- r2[!duplicated(r2),]
-    r3 = propagate_connectedDCTAs(r2)
+    r3 <- propagate_connectedDCTAs(r2)
     r3 <- r3[!duplicated(r3),]
-    r3 = merge(orthogroup.dt, r3, by = "id")
-    r4 = propagate_connectedOGs(r3)
+    r3 <- merge(og.dt, r3, by = "id")
+    r4 <- propagate_connectedOGs(r3)
     r4 <- r4[!duplicated(r4),]
     r4$array.id <- frank(r4$array.id, ties.method = "dense")
     return(r4)
@@ -148,8 +151,8 @@ find_pairwiseTandemArrays <- function(gff,
   prep_gff4mcscan <- cmpfun(prep_gff4mcscan)
   #######################################################
   #######################################################
-  if(rerank){
-    if(verbose)
+  if (rerank){
+    if (verbose)
       cat("Using the gff gene ranks as positions\n")
     rr = with(blast,
               rerank_fromIDs(id1 = id1,
@@ -157,54 +160,53 @@ find_pairwiseTandemArrays <- function(gff,
                              gff = gff))
     blast$rank1 <- NULL
     blast$rank2 <- NULL
-    blast <- merge(rr[,c("id1", "id2","rank1","rank2")],
+    blast <- merge(rr[,c("id1", "id2", "rank1", "rank2")],
                    blast,
                    by = c("id1","id2"))
 
   }
   if (verbose)
     cat("Running tandem array searches for each pair of genomes ... \n")
-  genome.cmbns = blast[,c("genome1","genome2")]
-  genome.cmbns = genome.cmbns[!duplicated(genome.cmbns),]
-  genome.cmbns = genome.cmbns[genome.cmbns$genome1 != genome.cmbns$genome2,]
+  genome.cmbns <- blast[, c("genome1", "genome2")]
+  genome.cmbns <- genome.cmbns[!duplicated(genome.cmbns), ]
+  genome.cmbns <- genome.cmbns[genome.cmbns$genome1 != genome.cmbns$genome2, ]
 
-  pw.cta = rbindlist(apply(genome.cmbns, 1, function(x){
+  pw.cta <- rbindlist(apply(genome.cmbns, 1, function(x){
     if (verbose)
-      cat(x[1],"<-->",x[2], "... ")
-    gff.in <- prep_gff4mcscan(gff = gff, genomeIDs = x)
-    blast.in <- prep_blast4mcscan(blast = blast, genomeIDs = x)
+      cat(x[1], "<-->", x[2], "... ")
+    gff.in <- prep_gff4mcscan(gff = gff,
+                              genomeIDs = x)
+    blast.in <- prep_blast4mcscan(blast = blast,
+                                  genomeIDs = x)
 
-    gff.file = file.path(mcscan.dir,"xyz.gff")
-    blast.file = file.path(mcscan.dir,"xyz.blast")
-    collin.file = file.path(mcscan.dir,"xyz.collinearity")
-    dcta.file = file.path(mcscan.dir,"xyz.TCA.txt")
+    gff.file <- file.path(mcscan.dir, "xyz.gff")
+    blast.file <- file.path(mcscan.dir, "xyz.blast")
+    collin.file <- file.path(mcscan.dir, "xyz.collinearity")
+    dcta.file <- file.path(mcscan.dir, "xyz.TCA.txt")
 
     write.table(gff.in,
                 file = gff.file,
                 row.names = F,
                 col.names = F,
-                quote = F, sep= "\t")
+                quote = F, sep = "\t")
     write.table(blast.in,
                 file = blast.file,
                 row.names = F,
                 col.names = F,
-                quote = F, sep= "\t")
+                quote = F, sep = "\t")
     if (verbose)
       cat("running MCScanX ... ")
 
-    system(paste("MCScanX -a",
-                 "-m", m.param,
-                 "-w", w.param,
-                 "-s", s.param,
-                 "-b", b.param,
-                 "-k", k.param,
-                 "-e", e.param,
-                 file.path(mcscan.dir,"xyz"), "&> /dev/null"))
+    com <- paste("MCScanX -a",
+                MCScanX.param,
+                file.path(mcscan.dir,"xyz"),
+                "&> /dev/null")
+    system(com)
 
     if (verbose)
       cat("running detect_collinear_tandem_arrays ... \n\t")
 
-    com = paste(DCTA.path,
+    com <- paste(DCTA.path,
                 "-b", blast.file,
                 "-g", gff.file,
                 "-c", collin.file,
@@ -212,24 +214,34 @@ find_pairwiseTandemArrays <- function(gff,
     system(com)
 
     dtca <- load_DCTAs(dcta.file,
-                       orthogroup.dt = orthogroup.dt[,c("id","og")])
+                       map = map)
     dtca$unique.genome <- paste(x[1], x[2])
     if (verbose)
       cat("Found",
-          length(unique(dtca$array.id)),"tandem arrays across",
+          length(unique(dtca$array.id)),
+          "tandem arrays across",
           nrow(dtca), "genes\n")
     return(dtca)
   }))
 
-  blast$unique.genome <- with(blast, paste(genome1, genome2))
+  blast$unique.genome <- with(blast,
+                              paste(genome1, genome2))
   pw.cta$og1 <- pw.cta$og
-  pw.cta$tandemarray.id <- with(pw.cta, paste(og,unique.genome))
-  pw.cta <- pw.cta[,c("tandemarray.id","og1","unique.genome")]
-  pw.cta <- pw.cta[!duplicated(pw.cta),]
+  pw.cta$tandemarray.id <- with(pw.cta,
+                                paste(og,
+                                      unique.genome,
+                                      block.id))
+  pw.cta <- pw.cta[,c("tandemarray.id", "og1", "unique.genome")]
+  pw.cta <- pw.cta[!duplicated(pw.cta), ]
+
   setkey(pw.cta, og1, unique.genome)
   setkey(blast, og1, unique.genome)
-  out = merge(blast, pw.cta, all = T)
+  out <- merge(blast,
+               pw.cta,
+               all = T)
+
   if (verbose)
     cat("\tDone!\n")
+
   return(out)
 }
