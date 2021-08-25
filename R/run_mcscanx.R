6621c5ce34e58367dc47ac21f5283cba91916eff
John Lovell <jlovell@lbl.gov>
2021-08-25 16:29:49 -0600

    v0.7 updates

---
+++ R/run_mcscanx.R
@@ -0,0 +1,136 @@
+#' @title run_mcscanx
+#'
+#' @description
+#' \code{run_mcscanx} run_mcscanx
+#'
+#' @param hits a data.table with at least ofID1, ofID2, chr1, chr2, ord1, ord2
+#' as columns
+#' @param nGaps integer of length 1, specifying the -m param to mcscanx
+#' for the primary MCScanX run. This acts on the results from the initial
+#' MCScanX run.
+#' @param path2mcscanx character string file.path pointing to the install
+#' directory for the MCScanX program. In particular, there must be an
+#' executable in this directory for MCScanX_h.
+#' @param blkSize integer of length 1, specifying the -s param to mcscanx
+#'
+#' @details Simple directory parser to find and check the paths to all
+#' annotation and assembly files.
+#'
+#' @return a named vector specifying the blocks and gene pairs therein
+#'
+#' @examples
+#' \dontrun{
+#' # coming soon
+#' }
+#' @import R.utils
+#' @import data.table
+#' @export
+run_mcscanx <- function(hits,
+                        blkSize,
+                        nGaps,
+                        path2mcscanx){
+  ##############################################################################
+  # parameter argument checking
+  gn2 <- mcsID1 <- mcsID2 <- blkID <- ord2 <- ofID1 <- ofID2 <- ord1 <- NULL
+  runBlast <- chr1 <- chr2 <- blkID <- NULL
+  hitCols <- c("ofID1","ofID2","chr1","chr2","ord1","ord2","score")
+  if(!all(hitCols %in% colnames(hits)))
+    stop("Looks like the hits data.table is misformed. It must be a data.table",
+         "comparing two genomes with the columns:", hitCols,"\n")
+  if(length(blkSize) != 1)
+    stop("blkSize must be a integer of length 1\n")
+  if(length(nGaps) != 1)
+    stop("blkSize must be a integer of length 1\n")
+  if(!is.integer(blkSize)){
+    blkSize <- as.integer(blkSize)
+    if(is.na(blkSize))
+      stop("Cannot coerce blkSize to an integer\n")
+  }
+  if(!is.integer(nGaps)){
+    nGaps <- as.integer(nGaps)
+    if(is.na(nGaps))
+      stop("Cannot coerce nGaps to an integer\n")
+  }
+  MCScanX_h <- file.path(path2mcscanx, "MCScanX_h")
+  if(!file.exists(MCScanX_h))
+    stop("Cannot find MCScanX_h executable in", path2mcscanx,"\n")
+
+  ##############################################################################
+  # set tmp directory
+  tmpDir <- file.path(
+    getwd(),
+    paste0("tmp_",
+           paste(sample(c(letters,LETTERS), 20, replace = T), collapse = "")))
+  if (dir.exists(tmpDir))
+    unlink(tmpDir, recursive = T)
+  dir.create(tmpDir)
+  on.exit(expr = unlink(tmpDir, recursive = T))
+
+  ##############################################################################
+  # convert gene locations to 'gff' like mcscanx file
+  u1 <- subset(hits, !duplicated(ofID1))
+  u2 <- subset(hits, !duplicated(ofID2))
+  setkey(u1, chr1, ord1)
+  setkey(u2, chr2, ord2)
+  u1[,mcsID1 := paste0("aa", as.numeric(factor(chr1, levels = unique(chr1))))]
+  u2[,mcsID2 := paste0("bb", as.numeric(factor(chr2, levels = unique(chr2))))]
+
+  mcs1 <- u1[,c("mcsID1", "ofID1","ord1","ord1")]
+  setnames(mcs1, c("chr", "id","start","end"))
+
+  mcs2 <- u2[,c("mcsID2", "ofID2","ord2","ord2")]
+  setnames(mcs2, c("chr", "id","start","end"))
+  mcsGffIn <- rbind(mcs1, mcs2)
+
+  ##############################################################################
+  # convert hits to mcscanx_h blast input
+  mcsBlsIn <- hits[,c("ofID1", "ofID2", "score")]
+  # mcsBlsIn[,score := 1]
+
+  mcsBlsIn[,ofID2 := paste0(ofID2,"xxxx")]
+  mcsGffIn$id[grepl("bb", mcsGffIn$chr)] <- paste0(mcsGffIn$id[grepl("bb", mcsGffIn$chr)],"xxxx")
+
+  blFile <- file.path(tmpDir, "mcs.homology")
+  gfFile <- file.path(tmpDir, "mcs.gff")
+  colFile <- file.path(tmpDir, "mcs.collinearity")
+
+  fwrite(
+    mcsGffIn,
+    file = gfFile,
+    sep = "\t",
+    quote = FALSE,
+    col.names = FALSE,
+    showProgress = FALSE,
+    verbose = FALSE)
+  fwrite(
+    mcsBlsIn,
+    file = blFile,
+    sep = "\t",
+    quote = FALSE,
+    col.names = FALSE,
+    showProgress = FALSE,
+    verbose = FALSE)
+
+  ##############################################################################
+  # run mcscanx_h
+  mcsCom <- sprintf(
+    "%s -a -b 2 -c 2 -m %s -s %s %s 1>/dev/null 2>&1",
+    MCScanX_h, nGaps, blkSize, file.path(tmpDir,"mcs"))
+  system(mcsCom)
+  ##############################################################################
+  idg <- strsplit(as.character(hits$ofID1[1]), "_")[[1]][1]
+  # parse collinearity file
+  suppressWarnings(collin <-  fread(
+    cmd = sprintf("cat %s | grep %s_ | grep :", colFile, idg),
+    col.names = c("blkID","gn1","gn2"),
+    select = 1:3,
+    header = F))
+  if(nrow(collin) > 1){
+    collin[,blkID := as.numeric(sapply(blkID, function(x)
+      strsplit(x, "-")[[1]][1])) + 1]
+    collin[,gn2 := gsub("xxxx$", "", gn2)]
+    mcsb <- collin$blkID
+    names(mcsb) <- with(collin, paste(gn1, gn2))
+    return(mcsb)
+  }
+}
