9a02ded7e329a3d29376e218a50816f253cdb026
jtlovell <johntlovell@gmail.com>
2022-10-13 17:11:06 -0600

    setting analysis threads to 1

--- R/run_orthofinder.R
+++ R/run_orthofinder.R
@@ -140,7 +140,7 @@ run_orthofinder <- function(gsParam,
     # 3. Get the orthofinder command
     ofComm <- sprintf(
       "-f %s -t %s -a 1 %s %s -X -o %s",
-      tmpDir, nCores, nCores,
+      tmpDir, nCores,
       ifelse(onewayBlast, "-1", ""),
       ifelse(diamondUltraSens, "-S diamond_ultra_sens", ""),
       ofDir)
